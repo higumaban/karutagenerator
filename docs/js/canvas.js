c22cb20150849413d844ec934b4673ed5346d4a4
インカ <inca@MacBookPro13-inca.local>
2018-02-11 17:58:55 +0900

    remove old canvas.js

--- docs/js/canvas.js
+++ docs/js/canvas.js
@@ -6,10 +6,18 @@ function draw() {
   if ( ! canvas || ! canvas.getContext ) { return false; }
   var ctx = canvas.getContext('2d');
   /* Imageオブジェクトを生成 */
-  var img = new Image();
-  img.src = "img/waku.png";
+
+  var bg_img = new Image();
+  bg_img.src = "img/min1.png";
   /* 画像を描画 */
-  img.onload = function(){
-    ctx.drawImage(img, 0, 0);
+  bg_img.onload = function(){
+    ctx.drawImage(bg_img, 0, 0);
   }
+
+  // var frame_img = new Image();
+  // frame_img.src = "img/waku.png";
+  // /* 画像を描画 */
+  // frame_img.onload = function(){
+  //   ctx.drawImage(frame_img, 0, 0);
+  // }
 }
