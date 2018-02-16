6fa41e17dfafd93c8b59240bf74552b6528e177f
インカ <inca@MacBookPro13-inca.local>
2018-02-17 08:06:44 +0900

    絵札選択画面テスト

--- docs/js/karutaselector.js
+++ docs/js/karutaselector.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
   document.documentElement.addEventListener('touchstart', function (e) {
     if (e.touches.length >= 2) {e.preventDefault();}
   }, false);
-
   var t = 0;
   document.documentElement.addEventListener('touchend', function (e) {
     var now = new Date().getTime();
@@ -11,6 +10,8 @@ $(document).ready(function(){
     }
     t = now;
   }, false);
+
+  var temp_accordion = "";
 
   $('.card_column').slick({
     // dots: true,
@@ -21,5 +22,62 @@ $(document).ready(function(){
     nextArrow: '<img src="./img/resized/arrow.png" class="slide-arrow prev-arrow">',
     slidesToShow: 3,
     slidesToScroll: 3
+  });
+
+  $('.yomifuda').on('click',function(){
+    var add_illust_selector = function(that){
+      var illust_selector =  $("<div></div>", {
+        "class": "card_column illust_selector"
+      });
+      illust_selector.append($("<img></img>", {
+        "class": "card efuda",
+        "value": $(that).attr("value"),
+        "src": "./img/resized/" + $(that).attr("value") + "_sentence.jpg"
+      }));
+      illust_selector.append($("<img></img>", {
+        "class": "card efuda",
+        "value": "ri",
+        "src": "./img/resized/mi_00.jpg"
+      }));
+      illust_selector.append($("<img></img>", {
+        "class": "card efuda",
+        "value": "ru",
+        "src": "./img/resized/mi_01.jpg"
+      }));
+      illust_selector.append($("<img></img>", {
+        "class": "card efuda",
+        "value": "re",
+        "src": "./img/resized/mi_02.jpg"
+      }));
+      illust_selector.append($("<img></img>", {
+        "class": "card efuda",
+        "value": "ro",
+        "src": "./img/resized/mi_03.jpg"
+      }));
+      illust_selector.slick({
+        arrows: true,
+        speed: 200,
+        draggable: false,
+        swipe: false,
+        nextArrow: '<img src="./img/resized/arrow_w.png" class="slide-arrow prev-arrow">',
+        slidesToShow: 3,
+        slidesToScroll: 3
+      });
+      $(that).parent().parent().parent().after(illust_selector);
+    }
+
+    if(temp_accordion == ""){
+      add_illust_selector(this);
+      temp_accordion = $(this).attr("value");
+    }else{
+      $(".illust_selector").remove();
+      if($(this).attr("value") != temp_accordion){
+        add_illust_selector(this);
+        temp_accordion = $(this).attr("value");
+      }else{
+        temp_accordion = "";
+      }
+    }
+
   });
 });
