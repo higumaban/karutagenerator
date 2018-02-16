c730a855748201f2d6ba37ddecfd2c5cfe517a0f
インカ <inca@MacBookPro13-inca.local>
2018-02-17 01:36:09 +0900

    絵札選択画面テスト

--- docs/js/karutaselector.js
+++ docs/js/karutaselector.js
@@ -1,4 +1,17 @@
 $(document).ready(function(){
+  document.documentElement.addEventListener('touchstart', function (e) {
+    if (e.touches.length >= 2) {e.preventDefault();}
+  }, false);
+
+  var t = 0;
+  document.documentElement.addEventListener('touchend', function (e) {
+    var now = new Date().getTime();
+    if ((now - t) < 350){
+      e.preventDefault();
+    }
+    t = now;
+  }, false);
+
   $('.card_column').slick({
     // dots: true,
     arrows: true,
